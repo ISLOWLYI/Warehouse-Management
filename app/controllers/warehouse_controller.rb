2fec7baf11fa9e6c5e8d742d6f968b93e242a27d
DenVaiR <fghfgh100.100@gmal.com>
2026-04-28 09:47:00 +0300

    Карта склада

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -1,7 +1,7 @@
 class WarehouseController < ApplicationController
   before_action :authenticate_user!
-  
+
   def info
-    @stock_items = ["Товар А - 100 шт", "Товар Б - 50 шт", "Товар В - 30 шт"]
+    redirect_to warehouses_path
   end
 end
